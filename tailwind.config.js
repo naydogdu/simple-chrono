4b781aa1edb95525b2ee912fdd2bf33e5c665087
Nazmi Aydogdu <nazmi@grapheek.com>
2020-07-03 11:50:28 +0200

    optimize config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  purge: [],
+  purge: {
+    enabled: true,
+    content: ['./src/**/*.js', './public/**/*.html'],
+  },
   theme: {
     fontFamily: {
       'mono': ['Orbitron', 'Menlo', 'Monaco', 'Consolas', '"Liberation Mono"', '"Courier New"', 'monospace']
